5daa99f97163adda1dfd95114026c74d77e4daf8
johnnybro360 <abest82@gmail.com>
2023-09-22 14:02:44 +0800

    post demonstration

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
 from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    tax: Optional[float] = None
+
 
 app = FastAPI()
 
@@ -9,6 +18,36 @@ async def home():
     return {'message': 'hello world'}
 
 
+@app.get('/items/')
+async def items_show():
+    return {'items': 'All items'}
+
+
+@app.post('/items/')
+async def item_add(item: Item):
+    return item
+
+
 @app.get('/items/{item_id}')
 async def items_show(item_id: int, search: Optional[str] = None):
     return {'items': f'{item_id} {search}'}
+
+
+@app.get('/users/')
+async def users_list():
+    return {"user_id": f"current user id"}
+
+
+@app.get('/users/{user_id}')
+async def users_read(user_id: int):
+    return {"user_id": f"{user_id}"}
+
+# @app.
+# get
+# post
+# put
+# patch
+# delete
+# options
+# head
+# trace
