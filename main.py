1b36f7c559cd7222b52b48fd98712d6af31cb986
johnnybro360 <abest82@gmail.com>
2023-09-22 13:34:30 +0800

    get demonstration

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
+
 
 @app.get('/')
 async def home():
     return {'message': 'hello world'}
+
+
+@app.get('/items/{item_id}')
+async def items_show(item_id: int, search: Optional[str] = None):
+    return {'items': f'{item_id} {search}'}
